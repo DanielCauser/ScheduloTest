42cec1ed1f0e19429d69ac5b19d8467cd02fd981
Daniel Causer <danielcauser@gmail.com>
2019-05-05 16:44:25 -0400

    #4 sending selected Post to post detail

--- ScheduloTestResolution/ScheduloTestResolution/ScheduloTestResolution/ViewModels/ViewModelForum.cs
+++ ScheduloTestResolution/ScheduloTestResolution/ScheduloTestResolution/ViewModels/ViewModelForum.cs
@@ -14,17 +14,26 @@ namespace ScheduloTestResolution.ViewModels
     public class ViewModelForum : ViewModelBase
     {
         private readonly IServiceForum _serviceForum;
+        private readonly INavigationService _navigationService;
 
-        public ViewModelForum(IServiceForum serviceForum)
+        public ViewModelForum(IServiceForum serviceForum,
+            INavigationService navigationService)
         {
             this._serviceForum = serviceForum;
-
+            this._navigationService = navigationService;
             this.Posts = new ObservableCollection<ModelPost>();
 
             this.LoadDataCommand = ReactiveCommand.Create<string>(filter =>
             {
                 Posts.Clear();
                 Posts.AddRange(_serviceForum.GetMatchingPosts(filter));
+            });
+
+            this.ItemTappedCommand = ReactiveCommand.CreateFromTask<ModelPost>(async selectedItem =>
+            {
+                var navigationParams = new NavigationParameters();
+                navigationParams.Add(nameof(ModelPost), selectedItem);
+                await _navigationService.NavigateAsync("ViewPost", navigationParams);
             });
 
             this.WhenAnyValue(x => x.SearchText)
@@ -34,6 +43,7 @@ namespace ScheduloTestResolution.ViewModels
         [Reactive] public string SearchText { get; set; }
         [Reactive] public ObservableCollection<ModelPost> Posts { get; set; }
 
+        public ICommand ItemTappedCommand { get; }
         public ICommand LoadDataCommand { get; }
     }
 }
--- ScheduloTestResolution/ScheduloTestResolution/ScheduloTestResolution/ViewModels/ViewModelPost.cs
+++ ScheduloTestResolution/ScheduloTestResolution/ScheduloTestResolution/ViewModels/ViewModelPost.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using Prism.Navigation;
+using ReactiveUI.Fody.Helpers;
+
 namespace ScheduloTestResolution.ViewModels
 {
-    public class ViewModelPost :ViewModelBase
+    public class ViewModelPost : ViewModelBase
     {
-        public ViewModelPost()
+        [Reactive] public ModelPost SelectedPost { get; set; }
+
+        public override void OnNavigatingTo(INavigationParameters parameters)
         {
+            if (parameters.TryGetValue<ModelPost>(nameof(ModelPost), out var selectedPost))
+                SelectedPost = selectedPost;
         }
     }
 }
